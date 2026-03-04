c0e8e04bce1339f627b92005e0383da52fc71b77
Anuska Biswas <anuskabiswas2027@gmail.com>
2026-03-04 12:28:33 +0530

    All models added

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+//const bcrypt = require("bcryptjs");
 const UserSchema=new mongoose.Schema(
     {
         name:{
@@ -27,4 +27,16 @@ const UserSchema=new mongoose.Schema(
     },
     {timestamps:true}
 );
+/* Hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}; 
+*/
 module.exports=mongoose.model('User',UserSchema);
